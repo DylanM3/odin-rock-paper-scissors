5246f19ab7daa0a8d33ca7cc6bb29c4bf9c404aa
Dylan Moore <dylanxmoore0@gmail.com>
2026-09-08 08:51:13 +0100

    0.2.0 - Random Computer Choice

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let randNum = Math.random();
+    
+    if (randNum <= 0.33333) {
+        return "Rock";
+    }
+    else if (randNum >= 0.66666) {
+        return "Paper";
+    }
+    else { return "Scissors"; }     
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Your Choice: ");
+    humanChoice.toLowerCase()
+    
+    if (humanChoice == "rock") {
+        return "Rock"
+    }
+    else if (humanChoice == "paper") {
+        return "Paper"
+    }
+    else if (humanChoice == "scissors") {
+        return "Scissors"
+    }
+    else { return "userInputError" }
+}
+
+const cpuChoice = getComputerChoice();
+console.log(cpuChoice);
+
+const humanChoice = getHumanChoice();
+console.log(humanChoice);
